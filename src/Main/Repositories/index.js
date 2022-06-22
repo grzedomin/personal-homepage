a2007d8311d85de349a0f05444c703835ba79c01
Grzegorz Domin <grze.domin98@gmail.com>
2022-06-22 22:38:05 +0200

    repositories displayed

--- src/Main/Repositories/index.js
+++ src/Main/Repositories/index.js
@@ -34,14 +34,18 @@ const Repositories = () => {
     return (
         <Repos>
             <>
-                <Tile>
-                    <Header>Movies Browser</Header>
-                    <Description>
-                        Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search.
-                    </Description>
-                    <DemoLink>Demo: </DemoLink>
-                    <RepoLink>Code: </RepoLink>
-                </Tile>
+                {repos.map((item) => {
+                    return (
+                        <Tile>
+                            <Header>Movies Browser</Header>
+                            <Description>
+                                Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search.
+                            </Description>
+                            <DemoLink>Demo: </DemoLink>
+                            <RepoLink>Code: </RepoLink>
+                        </Tile>
+                    )
+                })}
             </>
         </Repos>
     );
