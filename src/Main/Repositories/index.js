a6b863dc1b697f64d97809fd82e3e5019a76bdc2
Grzegorz Domin <grze.domin98@gmail.com>
2022-06-22 22:37:02 +0200

    github API used to get repositories

--- src/Main/Repositories/index.js
+++ src/Main/Repositories/index.js
@@ -1,37 +1,39 @@
 import { Repos, Tile, Header, Description, DemoLink, RepoLink } from "./styled";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 
 const Repositories = () => {
+
+    const [fetchState, setFetchState] = useState({
+        state: "loading",
+    });
+
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+        const fetchRepos = async () => {
+            try {
+                const response = await axios("https://api.github.com/users/grzedomin/repos");
+                setRepos(response.data);
+                setFetchState({
+                    state: "success",
+                });
+                console.log(response.data);
+            }
+            catch (error) {
+                setFetchState({
+                    state: "error",
+                });
+                console.log(error)
+            }
+        };
+        setTimeout(fetchRepos);
+    }, []);
 
     return (
         <Repos>
             <>
-                <Tile>
-                    <Header>Movies Browser</Header>
-                    <Description>
-                        Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search.
-                    </Description>
-                    <DemoLink>Demo: </DemoLink>
-                    <RepoLink>Code: </RepoLink>
-                </Tile>
-
-                <Tile>
-                    <Header>Movies Browser</Header>
-                    <Description>
-                        Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search.
-                    </Description>
-                    <DemoLink>Demo: </DemoLink>
-                    <RepoLink>Code: </RepoLink>
-                </Tile>
-
-                <Tile>
-                    <Header>Movies Browser</Header>
-                    <Description>
-                        Project description, e.g. website where you can search for favourite movies and people. Project description, e.g. website where you can search.
-                    </Description>
-                    <DemoLink>Demo: </DemoLink>
-                    <RepoLink>Code: </RepoLink>
-                </Tile>
-
                 <Tile>
                     <Header>Movies Browser</Header>
                     <Description>
