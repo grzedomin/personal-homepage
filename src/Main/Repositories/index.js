6e1b6e6e084ffc38976e118cd9545d79902a354d
Grzegorz Domin <grze.domin98@gmail.com>
2022-06-24 00:45:47 +0200

    fetchData moved to custom hook useFetchData

--- src/Main/Repositories/index.js
+++ src/Main/Repositories/index.js
@@ -1,35 +1,16 @@
 import Loading from "./Loading";
 import LoadingError from "./LoadingError";
 import { Repos, Tile, Header, Description, Demo, Repo, Link, LoadingInfo } from "./styled";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useFetchData } from "./useFetchData";
+
 
 
 const Repositories = () => {
 
-    const [fetchState, setFetchState] = useState({
-        state: "loading",
-    });
-    const [repos, setRepos] = useState([]);
-
-    useEffect(() => {
-        const fetchRepos = async () => {
-            try {
-                const response = await axios("https://api.github.com/users/grzedomin/repos");
-                setRepos(response.data);
-                setFetchState({
-                    state: "success",
-                });
-                console.log(response.data);
-            }
-            catch (error) {
-                setFetchState({
-                    state: "error",
-                });
-            }
-        };
-        setTimeout(fetchRepos, 2000);
-    }, []);
+    const {
+        fetchState,
+        repos
+    } = useFetchData();
 
     return (
         <>
---
+++ src/Main/Repositories/useFetchData.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useFetchData = () => {
+    
+    const [fetchState, setFetchState] = useState({
+        state: "loading",
+    });
+    const [repos, setRepos] = useState([]);
+    
+    useEffect(() => {
+        const fetchRepos = async () => {
+            try {
+                const response = await axios("https://api.github.com/users/grzedomin/repos");
+                setRepos(response.data);
+                setFetchState({
+                    state: "success",
+                });
+                console.log(response.data);
+            }
+            catch (error) {
+                setFetchState({
+                    state: "error",
+                });
+            }
+        };
+        setTimeout(fetchRepos, 2000);
+    }, []);
+    
+    return {
+        fetchState,
+        repos,
+    };
+};
