2148101175e47514e3bd5b190c57b0956770bec8
Grzegorz Domin <grze.domin98@gmail.com>
2022-06-27 11:13:02 +0200

    moved ThemeProvider and GlobalStyle to App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Skills from "./Skills";
 import Learn from "./Learn";
 import Main from "./Main";
 import Footer from "./Footer";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./theme";
+import { GlobalStyle } from './GlobalStyle';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
       <ThemeSwitch />
       <About />
       <Skills title="My skillset includes 🛠️" />
       <Learn title="What i want to learn next 🚀" />
       <Main title="Portfolio" />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { GlobalStyle } from './GlobalStyle';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
