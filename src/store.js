562b20313523be8b2826e174a1135045ca678f27
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-01 12:49:13 +0200

    sagaMiddleware store configured

--- src/store.js
+++ src/store.js
@@ -1,8 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./ThemeSwitch/themeSlice";
+import createSagaMiddleware from "redux-saga";
+import { watchFetchRepos } from "./Main/Repositories/reposSaga";
 
-export default configureStore({
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
     reducer: {
         theme: themeReducer,
-    }
+        
+    },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(watchFetchRepos);
+
+export default store;
