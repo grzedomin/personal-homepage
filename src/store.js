2d6fb0f6c84c7b9e4d99c1552871f0c244b97aca
Grzegorz Domin <grze.domin98@gmail.com>
2022-07-01 14:11:16 +0200

    store export changed

--- src/store.js
+++ src/store.js
@@ -1,18 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./ThemeSwitch/themeSlice";
-import createSagaMiddleware from "redux-saga";
-import { watchFetchRepos } from "./Main/Repositories/reposSaga";
-
-const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         theme: themeReducer,
         
     },
-    middleware: [sagaMiddleware],
 });
-
-sagaMiddleware.run(watchFetchRepos);
-
 export default store;
